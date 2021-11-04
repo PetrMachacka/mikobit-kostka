89dd2795d0dbce59d41d607b00d42bdca59fafb6
Petr Machačka <91247837+PetrMachacka@users.noreply.github.com>
2021-11-04 10:44:20 +0100

    tečky

--- main.py
+++ main.py
@@ -1,61 +1,49 @@
-stěny = 6
-můžeš = False
-def on_stěny():
+walls = 6
+ready = False
+def on_walls():
     if input.logo_is_pressed():
-        global stěny
-        if stěny == 6:
+        global walls
+        if walls == 6:
             basic.show_number(10)
-            stěny = 10
+            walls = 10
         else:
             basic.show_number(6)
-            stěny = 6
-basic.forever(on_stěny)
+            walls = 6
+basic.forever(on_walls)
 def on_můžeš():
     if input.button_is_pressed(Button.A):
-            global můžeš
+            global ready
             if můžeš == False:
                 basic.show_icon(IconNames.YES)
                 můžeš = True
 basic.forever(on_můžeš)
 def on_forever():
-        global můžeš, stěny
+        global ready, walls
         if můžeš == True:
             if input.is_gesture(Gesture.SHAKE):
-                tecky = randint(1, stěny)
-                if tecky == 1:
-                    basic.show_leds(""". . . . .
-                                    . . . . .
-                                    . . # . .
-                                    . . . . .
-                                    . . . . . """)
-                elif tecky == 2:
-                    basic.show_leds(""". . . . .
-                                    . . . . .
-                                    . # . # .
-                                    . . . . .
-                                    . . . . .""")
-                elif tecky == 3:
-                    basic.show_leds(""". . . . #
-                                    . . . . .
-                                    . . # . .
-                                    . . . . .
-                                    # . . . .""")
-                elif tecky == 4:
+                dots = randint(1, walls)
+                if dots == 1:
+                    basic.show_leds(""". . . . .. . . . .. . # . .. . . . .. . . . . """)
+                elif dots == 2:
+                    basic.show_leds(""". . . . . . . . . .. # . # .. . . . .. . . . .""")
+                elif dots == 3:
+                    basic.show_leds(""". . . . # . . . . .. . # . .. . . . .# . . . .""")
+                elif dots == 4:
                     basic.show_icon(IconNames.SMALL_DIAMOND)
-                elif tecky == 5:
-                    basic.show_icon(IconNames.YES)
-                elif tecky == 6:
+                elif dots == 5:
+                    basic.show_leds("""# . . . #. . . . .. . # . .. . . . .# . . . #""")
+                elif dots == 6:
                     basic.show_icon(IconNames.SMALL_HEART)
-                elif tecky == 7:
+                elif dots == 7:
                     basic.show_icon(IconNames.HAPPY)
-                elif tecky == 8:
+                elif dots == 8:
                     basic.show_icon(IconNames.SMALL_SQUARE)
-                elif tecky == 9:
+                elif dots == 9:
                     basic.show_icon(IconNames.NO)
-                elif tecky == 10:
+                elif dots == 10:
                     basic.show_icon(IconNames.TORTOISE)
-                for i in range(tecky):
-                    music.play_tone(Note.D, music.beat(3))
+                for i in range(dots):
+                    music.play_tone(Note.D, music.beat())
                     music.rest(music.beat(1.5))
                 můžeš = False
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,20 +1,21 @@
-let stěny = 6
-let můžeš = false
-basic.forever(function on_stěny() {
+let walls = 6
+let ready = false
+basic.forever(function on_walls() {
     if (input.logoIsPressed()) {
         
-        if (stěny == 6) {
+        if (walls == 6) {
             basic.showNumber(10)
-            stěny = 10
+            walls = 10
         } else {
             basic.showNumber(6)
-            stěny = 6
+            walls = 6
         }
         
     }
     
 })
 basic.forever(function on_můžeš() {
+    let můžeš: boolean;
     if (input.buttonIsPressed(Button.A)) {
         
         if (můžeš == false) {
@@ -26,47 +27,36 @@ basic.forever(function on_můžeš() {
     
 })
 basic.forever(function on_forever() {
-    let tecky: number;
+    let dots: number;
+    let můžeš: boolean;
     
     if (můžeš == true) {
         if (input.isGesture(Gesture.Shake)) {
-            tecky = randint(1, stěny)
-            if (tecky == 1) {
-                basic.showLeds(`. . . . .
-                                    . . . . .
-                                    . . # . .
-                                    . . . . .
-                                    . . . . . `)
-            } else if (tecky == 2) {
-                basic.showLeds(`. . . . .
-                                    . . . . .
-                                    . # . # .
-                                    . . . . .
-                                    . . . . .`)
-            } else if (tecky == 3) {
-                basic.showLeds(`. . . . #
-                                    . . . . .
-                                    . . # . .
-                                    . . . . .
-                                    # . . . .`)
-            } else if (tecky == 4) {
+            dots = randint(1, walls)
+            if (dots == 1) {
+                basic.showLeds(". . . . .. . . . .. . # . .. . . . .. . . . . ")
+            } else if (dots == 2) {
+                basic.showLeds(". . . . . . . . . .. # . # .. . . . .. . . . .")
+            } else if (dots == 3) {
+                basic.showLeds(". . . . # . . . . .. . # . .. . . . .# . . . .")
+            } else if (dots == 4) {
                 basic.showIcon(IconNames.SmallDiamond)
-            } else if (tecky == 5) {
-                basic.showIcon(IconNames.Yes)
-            } else if (tecky == 6) {
+            } else if (dots == 5) {
+                basic.showLeds("# . . . #. . . . .. . # . .. . . . .# . . . #")
+            } else if (dots == 6) {
                 basic.showIcon(IconNames.SmallHeart)
-            } else if (tecky == 7) {
+            } else if (dots == 7) {
                 basic.showIcon(IconNames.Happy)
-            } else if (tecky == 8) {
+            } else if (dots == 8) {
                 basic.showIcon(IconNames.SmallSquare)
-            } else if (tecky == 9) {
+            } else if (dots == 9) {
                 basic.showIcon(IconNames.No)
-            } else if (tecky == 10) {
+            } else if (dots == 10) {
                 basic.showIcon(IconNames.Tortoise)
             }
             
-            for (let i = 0; i < tecky; i++) {
-                music.playTone(Note.D, music.beat(3))
+            for (let i = 0; i < dots; i++) {
+                music.playTone(Note.D, music.beat())
                 music.rest(music.beat(1.5))
             }
             můžeš = false
